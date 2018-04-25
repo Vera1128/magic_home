936560d40d9632df71cf9ddc3cb7cf41eb53781c
Yangyang.Xu <yangyang.xu@cootek.cn>
2018-04-25 16:04:06 +0800

    上传白名单页面可运行版本1.0

--- es6/upload-white-list/js/upload-white-list.js
+++ es6/upload-white-list/js/upload-white-list.js
@@ -2,7 +2,7 @@
  * Created by yangyang.xu on 2018/4/24.
  */
 // const URI = 'https://dialer-manager.cootekservice.com';
-const URI = 'http://121.52.235.231:41211/';
+const URI = 'http://121.52.235.231:41211';
 var vue = new Vue({
   el: '#content',
   data: {
@@ -11,116 +11,55 @@ var vue = new Vue({
       enableOnScroll: true,
       listFinish: false,
       showBottomLoading: false,
-      showQueryGid: true
+      showQueryGid: true,
+      showMask: false
     },
-    whiteListGroup: [
-      {
-        id: 18,
-        owner: 'yangyang3',
-        date: '2018-04-24 23:00:00',
-        num: 10,
-        note: 'hahh这是我的测试数据',
-        operation: 1
-      },
-      {
-        id: 19,
-        owner: 'yangyang4',
-        date: '2018-04-24 23:00:00',
-        num: 10,
-        note: 'hahh这是我的测试数据',
-        operation: 1
-      },
-      {
-        id: 20,
-        owner: 'yangyang1',
-        date: '2018-04-24 23:00:00',
-        num: 10,
-        note: 'hahh这是我的测试数据',
-        operation: 1
-      },
-      {
-        id: 21,
-        owner: 'yangyang2',
-        date: '2018-04-24 23:00:00',
-        num: 10,
-        note: 'hahh这是我的测试数据',
-        operation: 1
-      },
-      {
-        id: 22,
-        owner: 'yangyang3',
-        date: '2018-04-24 23:00:00',
-        num: 10,
-        note: 'hahh这是我的测试数据',
-        operation: 1
-      },
-      {
-        id: 23,
-        owner: 'yangyang4',
-        date: '2018-04-24 23:00:00',
-        num: 10,
-        note: 'hahh这是我的测试数据',
-        operation: 1
-      },
-      {
-        id: 24,
-        owner: 'yangyang1',
-        date: '2018-04-24 23:00:00',
-        num: 10,
-        note: 'hahh这是我的测试数据',
-        operation: 1
-      },
-      {
-        id: 25,
-        owner: 'yangyang2',
-        date: '2018-04-24 23:00:00',
-        num: 10,
-        note: 'hahh这是我的测试数据',
-        operation: 1
-      },
-      {
-        id: 26,
-        owner: 'yangyang3',
-        date: '2018-04-24 23:00:00',
-        num: 10,
-        note: 'hahh这是我的测试数据',
-        operation: 1
-      },
-      {
-        id: 27,
-        owner: 'yangyang4',
-        date: '2018-04-24 23:00:00',
-        num: 10,
-        note: 'hahh这是我的测试数据',
-        operation: 1
-      }
-    ],
+    whiteListGroup: [],
     // 0为白名单列表 1为操作纪律列表
     selectListIndex: 0,
     startTime: '',
     endTime: '',
     note: '',
     endCursor: 0,
-    phoneStr: ''
+    queryGid: '',
+    phoneStr: '',
+    operationQuery: [],
+    changeInfoGid: 0
   },
   created () {
-    myLib.ajax({
-      type: 'get',
-      dataType: 'json',
-      url: URI + `/ad_whitelist_uploader/gid_query?cursor=${this.endCursor}`,
-      success: (res) => {
-
-      },
-      error: () => {
-        console.log('加此log用于测试');
-      }
-    });
+    this.init();
   },
   mounted () {
-    document.getElementById('content').style.display = 'block';
     window.onscroll = this.scroll;
   },
   methods: {
+    init () {
+      $.ajax({
+        type: 'get',
+        dataType: 'json',
+        url: URI + `/ad_whitelist_uploader/gid_query?cursor=${this.endCursor}`,
+        success: (res) => {
+          this.endCursor = res.end_cursor;
+          var gid_info = res.gid_info;
+          gid_info.forEach((item, index) => {
+            var obj = {};
+            var key = Object.keys(item)[0];
+            obj.gid = key;
+            obj.last_operator = item[key].last_operator;
+            obj.create_time = item[key].create_time;
+            obj.last_operatored_time = item[key].last_operatored_time;
+            obj.count = item[key].count;
+            obj.note = item[key].note;
+            obj.gid_status = item[key].gid_status;
+            this.whiteListGroup.push(obj);
+          })
+          document.getElementById('content').style.display = 'block';
+        },
+        error: () => {
+          console.log('加此log用于测试');
+        }
+      });
+    },
 //  上拉刷新 分页展示数据
     scroll (e) {
       var distance = document.body.scrollHeight - document.body.offsetHeight - window.scrollY;
@@ -133,18 +72,39 @@ var vue = new Vue({
       }
     },
     getNextPage () {
-      myLib.ajax({
+      $.ajax({
         type: 'get',
         dataType: 'json',
         url: URI + `/ad_whitelist_uploader/gid_query?cursor=${this.endCursor}`,
         success: (res) => {
-
+          console.log(JSON.stringify(res));
+          this.endCursor = res.end_cursor;
+          if (res.gid_info.length < 1) {
+            this.state.listFinish = true;
+          }
+          else {
+            var gid_info = res.gid_info;
+            gid_info.forEach((item, index) => {
+              var obj = {};
+              var key = Object.keys(item)[0];
+              obj.gid = key;
+              obj.last_operator = item[key].last_operator;
+              obj.create_time = item[key].create_time;
+              obj.last_operatored_time = item[key].last_operatored_time;
+              obj.count = item[key].count;
+              obj.note = item[key].note;
+              obj.gid_status = item[key].gid_status;
+              this.whiteListGroup.push(obj);
+            })
+            this.state.enableOnScroll = true;
+            this.state.showBottomLoading = false;
+          }
         },
         error: () => {
           console.log('加此log用于测试');
         }
       });
-      // myLib.ajax({
+      // $.ajax({
       //   url: URI + '/fellow_townsman/ops_get_spider_info?_token=' + TOKEN,
       //   type: 'post',
       //   dataType: 'json',
@@ -180,6 +140,7 @@ var vue = new Vue({
     },
     uploadWhiteList () {
       var file = document.getElementById('file').files[0];
+      console.log(file);
       if (!file) {
         alert('请选择文件上传');
       }
@@ -187,7 +148,7 @@ var vue = new Vue({
         var formData = new FormData();
         formData.append('file', file);
         formData.append('note', this.note || '无');
-        myLib.ajax({
+        $.ajax({
           type: 'post',
           dataType: 'json',
           data: formData,
@@ -196,7 +157,12 @@ var vue = new Vue({
           processData: false,
           url: URI + '/ad_whitelist_uploader/file_upload',
           success: (res) => {
-
+            if (res.gid == 0) {
+              alert('上传的文件内无合法号码');
+              return;
+            }
+            alert(`添加成功，gid为${res.gid}`);
+            location.reload();
           },
           error: () => {
 
@@ -214,9 +180,189 @@ var vue = new Vue({
     },
     goToOperationList () {
       this.selectListIndex = 1;
+      window.onscroll = null;
     },
     backToWhiteList () {
       this.selectListIndex = 0;
+      window.onscroll = this.scroll;
+    },
+    closeMask () {
+      this.state.showMask = false;
+    },
+    openMask (gid) {
+      this.state.showMask = true;
+      this.changeInfoGid = gid;
+    },
+    queryOperationList (flag) {
+      if (flag == '2') {
+        if (!this.startTime || !this.endTime) {
+          alert('请填写完整的开始时间/结束时间!');
+          return;
+        }
+        this.operationQuery = [];
+        $.ajax({
+          url: URI + '/ad_whitelist_uploader/operation_query',
+          type:'post',
+          data: JSON.stringify({
+            query_type: 2,
+            begin_time: this.startTime,
+            end_time: this.endTime
+          }),
+          dataType: 'json',
+          success: (res) => {
+            console.log(JSON.stringify(res));
+          }
+        });
+      }
+      else {
+        if (!this.queryGid) {
+          alert('请输入gid');
+          return;
+        }
+        this.operationQuery = [];
+        $.ajax({
+          url: URI + '/ad_whitelist_uploader/operation_query',
+          type:'post',
+          data: JSON.stringify({
+            query_type: 1,
+            gid: this.queryGid
+          }),
+          dataType: 'json',
+          success: (res) => {
+            console.log(JSON.stringify(res));
+            res.forEach((item, index) => {
+              var obj = {};
+              var key = Object.keys(item)[0];
+              obj.gid = key;
+              obj.operator = item[key].operator;
+              obj.operated_time = item[key].create_time;
+              switch (item[key].operated_status) {
+                case 0:
+                  obj.operation = '创建';
+                  break;
+                case 1:
+                  obj.operation = '失效';
+                  break;
+                case 2:
+                  obj.operation = '生效';
+                  break;
+                default:
+                  obj.operation = '追加';
+              }
+              this.operationQuery.push(obj);
+              console.log(this.operationQuery);
+            })
+          }
+        });
+      }
+    },
+    changeGidInfo (flag, gid) {
+      if (flag != '3') {
+        $.ajax({
+          url: URI + '/ad_whitelist_uploader/change_gid_info',
+          type:'post',
+          data: JSON.stringify({
+            change_type: parseInt(flag),
+            gid: gid
+          }),
+          dataType: 'json',
+          success: (res) => {
+            console.log(res);
+          }
+        });
+      }
+    },
+    // operation_query接口数据格式化
+    queryResult () {
+      // hard code
+      var data = {
+        '1': {
+          'operator': 'yang',
+          'operated_time': '2018-05-20',
+          'operated_status': 0
+        },
+        '2': {
+          'operator': '1',
+          'operated_time': '2018-05-20',
+          'operated_status': 1
+        },
+        '3': {
+          'operator': '2',
+          'operated_time': '2018-05-20',
+          'operated_status': 2
+        },
+        '4': {
+          'operator': '3',
+          'operated_time': '2018-05-20',
+          'operated_status': 3
+        }
+      };
+      var keys = Object.keys(data);
+      for (let i = 0; i < keys.length; i++) {
+        let obj = {};
+        obj.gid = keys[i];
+        obj.operator = data[keys[i]].operator;
+        obj.operated_time = data[keys[i]].operated_time;
+        switch (data[keys[i]].operated_status) {
+          case 0:
+            obj.operation = '创建';
+            break;
+          case 1:
+            obj.operation = '失效';
+            break;
+          case 2:
+            obj.operation = '生效';
+            break;
+          default:
+            obj.operation = '追加';
+        }
+        this.operationQuery.push(obj);
+      }
+    },
+    // gid_query接口数据格式化
+    gidResult () {
+      // hard code
+      var data = {
+        '1': {
+          'last_operator': 'yang',
+          'last_operated_time': '2018-05-20',
+          'gid_status': 0,
+          'count': 20,
+          'note': 'hhaha这是杨杨的测试数据'
+        },
+        '2': {
+          'last_operator': 'yang1',
+          'last_operated_time': '2018-05-20',
+          'gid_status': 1,
+          'count': 10,
+          'note': 'hhaha这是yang1的测试数据'
+        },
+        '3': {
+          'last_operator': 'yang2',
+          'last_operated_time': '2018-05-20',
+          'gid_status': 1,
+          'count': 2,
+          'note': 'hhaha这是yang2的测试数据'
+        },
+        '4': {
+          'last_operator': 'yang3',
+          'last_operated_time': '2018-05-20',
+          'gid_status': 0,
+          'count': 100,
+          'note': 'hhaha这是yang3的测试数据'
+        }
+      };
+      var keys = Object.keys(data);
+      for (let i = 0; i < keys.length; i++) {
+        let obj = {};
+        obj.gid = keys[i];
+        obj.last_operator = data[keys[i]].last_operator;
+        obj.last_operated_time = data[keys[i]].last_operated_time;
+        obj.count = data[keys[i]].count;
+        obj.note = data[keys[i]].note;
+        obj.gid_status = data[keys[i]].gid_status;
+        this.whiteListGroup.push(obj);
+      }
     }
   }
 });
--- templates/upload-white-list/js/upload-white-list.js
+++ templates/upload-white-list/js/upload-white-list.js
@@ -4,7 +4,7 @@
  * Created by yangyang.xu on 2018/4/24.
  */
 // const URI = 'https://dialer-manager.cootekservice.com';
-var URI = 'http://121.52.235.231:41211/';
+var URI = 'http://121.52.235.231:41211';
 var vue = new Vue({
   el: '#content',
   data: {
@@ -13,128 +13,107 @@ var vue = new Vue({
       enableOnScroll: true,
       listFinish: false,
       showBottomLoading: false,
-      showQueryGid: true
+      showQueryGid: true,
+      showMask: false
     },
-    whiteListGroup: [{
-      id: 18,
-      owner: 'yangyang3',
-      date: '2018-04-24 23:00:00',
-      num: 10,
-      note: 'hahh这是我的测试数据',
-      operation: 1
-    }, {
-      id: 19,
-      owner: 'yangyang4',
-      date: '2018-04-24 23:00:00',
-      num: 10,
-      note: 'hahh这是我的测试数据',
-      operation: 1
-    }, {
-      id: 20,
-      owner: 'yangyang1',
-      date: '2018-04-24 23:00:00',
-      num: 10,
-      note: 'hahh这是我的测试数据',
-      operation: 1
-    }, {
-      id: 21,
-      owner: 'yangyang2',
-      date: '2018-04-24 23:00:00',
-      num: 10,
-      note: 'hahh这是我的测试数据',
-      operation: 1
-    }, {
-      id: 22,
-      owner: 'yangyang3',
-      date: '2018-04-24 23:00:00',
-      num: 10,
-      note: 'hahh这是我的测试数据',
-      operation: 1
-    }, {
-      id: 23,
-      owner: 'yangyang4',
-      date: '2018-04-24 23:00:00',
-      num: 10,
-      note: 'hahh这是我的测试数据',
-      operation: 1
-    }, {
-      id: 24,
-      owner: 'yangyang1',
-      date: '2018-04-24 23:00:00',
-      num: 10,
-      note: 'hahh这是我的测试数据',
-      operation: 1
-    }, {
-      id: 25,
-      owner: 'yangyang2',
-      date: '2018-04-24 23:00:00',
-      num: 10,
-      note: 'hahh这是我的测试数据',
-      operation: 1
-    }, {
-      id: 26,
-      owner: 'yangyang3',
-      date: '2018-04-24 23:00:00',
-      num: 10,
-      note: 'hahh这是我的测试数据',
-      operation: 1
-    }, {
-      id: 27,
-      owner: 'yangyang4',
-      date: '2018-04-24 23:00:00',
-      num: 10,
-      note: 'hahh这是我的测试数据',
-      operation: 1
-    }],
+    whiteListGroup: [],
     // 0为白名单列表 1为操作纪律列表
     selectListIndex: 0,
     startTime: '',
     endTime: '',
     note: '',
     endCursor: 0,
-    phoneStr: ''
+    queryGid: '',
+    phoneStr: '',
+    operationQuery: [],
+    changeInfoGid: 0
   },
   created: function created() {
-    myLib.ajax({
-      type: 'get',
-      dataType: 'json',
-      url: URI + ('/ad_whitelist_uploader/gid_query?cursor=' + this.endCursor),
-      success: function success(res) {},
-      error: function error() {
-        console.log('加此log用于测试');
-      }
-    });
+    this.init();
   },
   mounted: function mounted() {
-    document.getElementById('content').style.display = 'block';
     window.onscroll = this.scroll;
   },
 
   methods: {
+    init: function init() {
+      var _this = this;
+
+      $.ajax({
+        type: 'get',
+        dataType: 'json',
+        url: URI + ('/ad_whitelist_uploader/gid_query?cursor=' + this.endCursor),
+        success: function success(res) {
+          _this.endCursor = res.end_cursor;
+          var gid_info = res.gid_info;
+          gid_info.forEach(function (item, index) {
+            var obj = {};
+            var key = Object.keys(item)[0];
+            obj.gid = key;
+            obj.last_operator = item[key].last_operator;
+            obj.create_time = item[key].create_time;
+            obj.last_operatored_time = item[key].last_operatored_time;
+            obj.count = item[key].count;
+            obj.note = item[key].note;
+            obj.gid_status = item[key].gid_status;
+            _this.whiteListGroup.push(obj);
+          });
+          document.getElementById('content').style.display = 'block';
+        },
+        error: function error() {
+          console.log('加此log用于测试');
+        }
+      });
+    },
+
     //  上拉刷新 分页展示数据
     scroll: function scroll(e) {
-      var _this = this;
+      var _this2 = this;
 
       var distance = document.body.scrollHeight - document.body.offsetHeight - window.scrollY;
       if (distance < 10 && this.state.enableOnScroll && !this.state.listFinish) {
         this.state.enableOnScroll = false;
         this.state.showBottomLoading = true;
         setTimeout(function () {
-          _this.getNextPage();
+          _this2.getNextPage();
         }, 500);
       }
     },
     getNextPage: function getNextPage() {
-      myLib.ajax({
+      var _this3 = this;
+
+      $.ajax({
         type: 'get',
         dataType: 'json',
         url: URI + ('/ad_whitelist_uploader/gid_query?cursor=' + this.endCursor),
-        success: function success(res) {},
+        success: function success(res) {
+          console.log(JSON.stringify(res));
+          _this3.endCursor = res.end_cursor;
+          if (res.gid_info.length < 1) {
+            _this3.state.listFinish = true;
+          } else {
+            var gid_info = res.gid_info;
+            gid_info.forEach(function (item, index) {
+              var obj = {};
+              var key = Object.keys(item)[0];
+              obj.gid = key;
+              obj.last_operator = item[key].last_operator;
+              obj.create_time = item[key].create_time;
+              obj.last_operatored_time = item[key].last_operatored_time;
+              obj.count = item[key].count;
+              obj.note = item[key].note;
+              obj.gid_status = item[key].gid_status;
+              _this3.whiteListGroup.push(obj);
+            });
+            _this3.state.enableOnScroll = true;
+            _this3.state.showBottomLoading = false;
+          }
+        },
         error: function error() {
           console.log('加此log用于测试');
         }
       });
-      // myLib.ajax({
+      // $.ajax({
       //   url: URI + '/fellow_townsman/ops_get_spider_info?_token=' + TOKEN,
       //   type: 'post',
       //   dataType: 'json',
@@ -170,13 +149,14 @@ var vue = new Vue({
     },
     uploadWhiteList: function uploadWhiteList() {
       var file = document.getElementById('file').files[0];
+      console.log(file);
       if (!file) {
         alert('请选择文件上传');
       } else {
         var formData = new FormData();
         formData.append('file', file);
         formData.append('note', this.note || '无');
-        myLib.ajax({
+        $.ajax({
           type: 'post',
           dataType: 'json',
           data: formData,
@@ -184,7 +164,14 @@ var vue = new Vue({
           contentType: false,
           processData: false,
           url: URI + '/ad_whitelist_uploader/file_upload',
-          success: function success(res) {},
+          success: function success(res) {
+            if (res.gid == 0) {
+              alert('上传的文件内无合法号码');
+              return;
+            }
+            alert('\u6DFB\u52A0\u6210\u529F\uFF0Cgid\u4E3A' + res.gid);
+            location.reload();
+          },
           error: function error() {}
         });
       }
@@ -199,9 +186,192 @@ var vue = new Vue({
     },
     goToOperationList: function goToOperationList() {
       this.selectListIndex = 1;
+      window.onscroll = null;
     },
     backToWhiteList: function backToWhiteList() {
       this.selectListIndex = 0;
+      window.onscroll = this.scroll;
+    },
+    closeMask: function closeMask() {
+      this.state.showMask = false;
+    },
+    openMask: function openMask(gid) {
+      this.state.showMask = true;
+      this.changeInfoGid = gid;
+    },
+    queryOperationList: function queryOperationList(flag) {
+      var _this4 = this;
+
+      if (flag == '2') {
+        if (!this.startTime || !this.endTime) {
+          alert('请填写完整的开始时间/结束时间!');
+          return;
+        }
+        this.operationQuery = [];
+        $.ajax({
+          url: URI + '/ad_whitelist_uploader/operation_query',
+          type: 'post',
+          data: JSON.stringify({
+            query_type: 2,
+            begin_time: this.startTime,
+            end_time: this.endTime
+          }),
+          dataType: 'json',
+          success: function success(res) {
+            console.log(JSON.stringify(res));
+          }
+        });
+      } else {
+        if (!this.queryGid) {
+          alert('请输入gid');
+          return;
+        }
+        this.operationQuery = [];
+        $.ajax({
+          url: URI + '/ad_whitelist_uploader/operation_query',
+          type: 'post',
+          data: JSON.stringify({
+            query_type: 1,
+            gid: this.queryGid
+          }),
+          dataType: 'json',
+          success: function success(res) {
+            console.log(JSON.stringify(res));
+            res.forEach(function (item, index) {
+              var obj = {};
+              var key = Object.keys(item)[0];
+              obj.gid = key;
+              obj.operator = item[key].operator;
+              obj.operated_time = item[key].create_time;
+              switch (item[key].operated_status) {
+                case 0:
+                  obj.operation = '创建';
+                  break;
+                case 1:
+                  obj.operation = '失效';
+                  break;
+                case 2:
+                  obj.operation = '生效';
+                  break;
+                default:
+                  obj.operation = '追加';
+              }
+              _this4.operationQuery.push(obj);
+              console.log(_this4.operationQuery);
+            });
+          }
+        });
+      }
+    },
+    changeGidInfo: function changeGidInfo(flag, gid) {
+      if (flag != '3') {
+        $.ajax({
+          url: URI + '/ad_whitelist_uploader/change_gid_info',
+          type: 'post',
+          data: JSON.stringify({
+            change_type: parseInt(flag),
+            gid: gid
+          }),
+          dataType: 'json',
+          success: function success(res) {
+            console.log(res);
+          }
+        });
+      }
+    },
+
+    // operation_query接口数据格式化
+    queryResult: function queryResult() {
+      // hard code
+      var data = {
+        '1': {
+          'operator': 'yang',
+          'operated_time': '2018-05-20',
+          'operated_status': 0
+        },
+        '2': {
+          'operator': '1',
+          'operated_time': '2018-05-20',
+          'operated_status': 1
+        },
+        '3': {
+          'operator': '2',
+          'operated_time': '2018-05-20',
+          'operated_status': 2
+        },
+        '4': {
+          'operator': '3',
+          'operated_time': '2018-05-20',
+          'operated_status': 3
+        }
+      };
+      var keys = Object.keys(data);
+      for (var i = 0; i < keys.length; i++) {
+        var obj = {};
+        obj.gid = keys[i];
+        obj.operator = data[keys[i]].operator;
+        obj.operated_time = data[keys[i]].operated_time;
+        switch (data[keys[i]].operated_status) {
+          case 0:
+            obj.operation = '创建';
+            break;
+          case 1:
+            obj.operation = '失效';
+            break;
+          case 2:
+            obj.operation = '生效';
+            break;
+          default:
+            obj.operation = '追加';
+        }
+        this.operationQuery.push(obj);
+      }
+    },
+
+    // gid_query接口数据格式化
+    gidResult: function gidResult() {
+      // hard code
+      var data = {
+        '1': {
+          'last_operator': 'yang',
+          'last_operated_time': '2018-05-20',
+          'gid_status': 0,
+          'count': 20,
+          'note': 'hhaha这是杨杨的测试数据'
+        },
+        '2': {
+          'last_operator': 'yang1',
+          'last_operated_time': '2018-05-20',
+          'gid_status': 1,
+          'count': 10,
+          'note': 'hhaha这是yang1的测试数据'
+        },
+        '3': {
+          'last_operator': 'yang2',
+          'last_operated_time': '2018-05-20',
+          'gid_status': 1,
+          'count': 2,
+          'note': 'hhaha这是yang2的测试数据'
+        },
+        '4': {
+          'last_operator': 'yang3',
+          'last_operated_time': '2018-05-20',
+          'gid_status': 0,
+          'count': 100,
+          'note': 'hhaha这是yang3的测试数据'
+        }
+      };
+      var keys = Object.keys(data);
+      for (var i = 0; i < keys.length; i++) {
+        var obj = {};
+        obj.gid = keys[i];
+        obj.last_operator = data[keys[i]].last_operator;
+        obj.last_operated_time = data[keys[i]].last_operated_time;
+        obj.count = data[keys[i]].count;
+        obj.note = data[keys[i]].note;
+        obj.gid_status = data[keys[i]].gid_status;
+        this.whiteListGroup.push(obj);
+      }
     }
   }
 });
